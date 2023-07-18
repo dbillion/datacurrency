4f733c0af2c017a6981e16f79bfafd55c4af0822
dbillion <dayozoe@gmail.com>
2023-07-18 23:11:53 +0300

    pom file change and lets see how it goes???????????????

--- src/test/java/com/dayo/tutorial/DatacurrencyApplicationTests.java
+++ src/test/java/com/dayo/tutorial/DatacurrencyApplicationTests.java
@@ -1,13 +1,52 @@
 package com.dayo.tutorial;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DatacurrencyApplicationTests {
 
+	@Autowired
+
+	DataController dataController;
+
 	@Test
-	void contextLoads() {
+
+	void health() {
+
+		assertEquals("HEALTH CHECK OK!", dataController.healthCheck());
+
+	}
+
+	@Test
+
+	void version() {
+
+		assertEquals("The actual version is 1.0.0", dataController.version());
+
+	}
+
+	@Test
+
+	void nationsLength() {
+
+		Integer nationsLength = dataController.getRandomNations().size();
+
+		assertEquals(10, nationsLength);
+
+	}
+
+	@Test
+
+	void currenciesLength() {
+
+		Integer currenciesLength = dataController.getRandomCurrencies().size();
+
+		assertEquals(20, currenciesLength);
+
 	}
 
 }
